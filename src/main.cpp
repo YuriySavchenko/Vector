5267f6c8ed3730ca1d148a20072ea93ae538d0b7
savchenko.yurii <savchenko.yurii@globallogic.com>
2019-09-25 21:19:29 +0300

    have been changed tabs to white spaces

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,30 @@
 #include "vector.h"
 
 int main(int argc, char *argv[]) {
-	Vector<int> v;
+    Vector<int> v;
 
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
-	v.push_back(4);
-	v.push_back(5);
-	v.insert(0, 40);
-	v.insert(2, 20);
-	v.insert(7, 30);
-	v.insert(5, 50);
-	v.erase(0);
-	v.erase(1);
-	v.erase(3);
-	v.erase(5);
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    v.insert(0, 40);
+    v.insert(2, 20);
+    v.insert(7, 30);
+    v.insert(5, 50);
+    v.erase(0);
+    v.erase(1);
+    v.erase(3);
+    v.erase(5);
 
-	for (int i=0; i < v.size(); i++) {
-		std::cout << v[i] << " ";
-	}
+    for (int i=0; i < v.size(); i++) {
+        std::cout << v[i] << " ";
+    }
 
-	std::cout << std::endl;
+    std::cout << std::endl;
 
-	std::cout << "size: " << v.size() << std::endl;
-	std::cout << "capacity: " << v.capacity() << std::endl;
+    std::cout << "size: " << v.size() << std::endl;
+    std::cout << "capacity: " << v.capacity() << std::endl;
 
-	return 0;
+    return 0;
 }
