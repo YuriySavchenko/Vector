9d50e21e213124602ae2066f7ec57df022ae850a
savchenko.yurii <savchenko.yurii@globallogic.com>
2019-09-26 19:58:59 +0300

    has been added changes into the testing methods for more careful checking

--- test/src/sources/testvector.cpp
+++ test/src/sources/testvector.cpp
@@ -2,11 +2,11 @@
 
 /* method for testing function Vector<T>::size() */
 void TestVector::testSize() {
-    TS_ASSERT_EQUALS(vINT.size(), 3);
+    TS_ASSERT_EQUALS(vINT.size(), 6);
     TS_ASSERT_EQUALS(vDOUBLE.size(), 4);
-    TS_ASSERT_EQUALS(vCHAR.size(), 2);
-    TS_ASSERT_EQUALS(vBOOL.size(), 5);
-    TS_ASSERT_EQUALS(vSTRING.size(), 4);
+    TS_ASSERT_EQUALS(vCHAR.size(), 4);
+    TS_ASSERT_EQUALS(vBOOL.size(), 4);
+    TS_ASSERT_EQUALS(vSTRING.size(), 3);
 }
 
 /* method for testing function Vector<T>::capacity() */
@@ -20,9 +20,9 @@ void TestVector::testCapacity() {
 
 /* method for testing function Vector<T>::empty() */
 void TestVector::testEmpty() {
-    TS_ASSERT_EQUALS(vINT.empty(), true);
+    TS_ASSERT_EQUALS(vINT.empty(), false);
     TS_ASSERT_EQUALS(vDOUBLE.empty(), false);
-    TS_ASSERT_EQUALS(vCHAR.empty(), true);
+    TS_ASSERT_EQUALS(vCHAR.empty(), false);
     TS_ASSERT_EQUALS(vBOOL.empty(), false);
     TS_ASSERT_EQUALS(vSTRING.empty(), false);
 }
@@ -32,35 +32,35 @@ void TestVector::testAt() {
     TS_ASSERT_EQUALS(vINT.at(4), 5);
     TS_ASSERT_EQUALS(vDOUBLE.at(3), 4.4);
     TS_ASSERT_EQUALS(vCHAR.at(2), 'c');
-    TS_ASSERT_EQUALS(vBOOL.at(1), false);
+    TS_ASSERT_EQUALS(vBOOL.at(1), true);
     TS_ASSERT_EQUALS(vSTRING.at(0), "programming");
 }
 
 /* method for testing function Vector<T>::front() */
 void TestVector::testFront() {
     TS_ASSERT_EQUALS(vINT.front(), 1);
-    TS_ASSERT_EQUALS(vDOUBLE.front(), 1.2);
+    TS_ASSERT_EQUALS(vDOUBLE.front(), 1.1);
     TS_ASSERT_EQUALS(vCHAR.front(), 'a');
-    TS_ASSERT_EQUALS(vBOOL.front(), true);
-    TS_ASSERT_EQUALS(vSTRING.front(), "so");
+    TS_ASSERT_EQUALS(vBOOL.front(), false);
+    TS_ASSERT_EQUALS(vSTRING.front(), "programming");
 }
 
 /* method for testing function Vector<T>::back() */
 void TestVector::testBack() {
-    TS_ASSERT_EQUALS(vINT.back(), 5);
-    TS_ASSERT_EQUALS(vDOUBLE.back(), 4.4);
+    TS_ASSERT_EQUALS(vINT.back(), 4);
+    TS_ASSERT_EQUALS(vDOUBLE.back(), 4.3);
     TS_ASSERT_EQUALS(vCHAR.back(), 'c');
-    TS_ASSERT_EQUALS(vBOOL.back(), true);
-    TS_ASSERT_EQUALS(vSTRING.back(), "cool");
+    TS_ASSERT_EQUALS(vBOOL.back(), false);
+    TS_ASSERT_EQUALS(vSTRING.back(), "cold");
 }
 
 /* method for testing function Vector<T>::operator[] */
 void TestVector::testOperator() {
     TS_ASSERT_EQUALS(vINT[4], 6);
     TS_ASSERT_EQUALS(vDOUBLE[3], 3.3);
-    TS_ASSERT_EQUALS(vCHAR[2], 'c');
-    TS_ASSERT_EQUALS(vBOOL[1], true);
-    TS_ASSERT_EQUALS(vSTRING[0], "programming");
+    TS_ASSERT_EQUALS(vCHAR[2], '0');
+    TS_ASSERT_EQUALS(vBOOL[1], false);
+    TS_ASSERT_EQUALS(vSTRING[0], "programmers");
 }
 
 /* method for testing function Vector<T>::push_back() */
@@ -72,9 +72,9 @@ void TestVector::testPushBack() {
     vSTRING.push_back("!");
 
     TS_ASSERT_EQUALS(vINT[5], 6);
-    TS_ASSERT_EQUALS(vDOUBLE[4], 4.4);
-    TS_ASSERT_EQUALS(vCHAR[4], 'd');
-    TS_ASSERT_EQUALS(vBOOL[4], true);
+    TS_ASSERT_EQUALS(vDOUBLE[4], 5.5);
+    TS_ASSERT_EQUALS(vCHAR[4], 'e');
+    TS_ASSERT_EQUALS(vBOOL[4], false);
     TS_ASSERT_EQUALS(vSTRING[3], "!");
 }
 
@@ -86,11 +86,11 @@ void TestVector::testErase() {
     vBOOL.erase(0);
     vSTRING.erase(3);
 
-    TS_ASSERT_EQUALS(vINT[4], 6);
+    TS_ASSERT_EQUALS(vINT[4], 5);
     TS_ASSERT_EQUALS(vDOUBLE[3], 5.5);
     TS_ASSERT_EQUALS(vCHAR[1], 'c');
     TS_ASSERT_EQUALS(vBOOL[0], true);
-    TS_ASSERT_EQUALS(vSTRING[2], "!");
+    TS_ASSERT_EQUALS(vSTRING[2], "cool");
 }
 
 /* method for testing function Vector<T>::insert() */
