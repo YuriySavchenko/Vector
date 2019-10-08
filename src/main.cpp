a5b84763ad63f6ac4c78fa678bc56983624c3eca
yurii.savchenko <yurii.savchenko@globallogic.com>
2019-10-08 19:52:08 +0300

    have been added class User for additional testing of Vector

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,31 @@
-#include <iostream>
 #include "vector.h"
 
 class User {
+    int id;
+    static int s_nextId;
 public:
-    User() {
-        std::cout << "User constructor" << std::endl;
+    User() :
+    id(s_nextId++) {
+            std::cout << "User " << id << " constructed." << std::endl;
     }
 
     ~User() {
-        std::cout << "User destructor" << std::endl;
+        std::cout << "User " << id << " destroyed" << std::endl;
     }
 };
 
-void add_user(Vector<User> & vector) {
-    User user;
-    vector.push_back(user);
+int User::s_nextId = 0;
+
+void add_user(Vector<User>& vector) {
+    for (int i = 0; i < 2; ++i) {
+        User user;
+        vector.push_back(user);
+    }
 }
 
 int main(int argc, char *argv[]) {
     Vector<User> users;
     add_user(users);
-
-    Vector<int> v;
-
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    v.push_back(4);
-    v.push_back(5);
-    v.insert(0, 40);
-    v.insert(2, 20);
-    v.insert(7, 30);
-    v.insert(5, 50);
-    v.erase(0);
-    v.erase(1);
-    v.erase(3);
-    v.erase(5);
-
-    for (int i=0; i < v.size(); i++) {
-        std::cout << v[i] << " ";
-    }
-
-    std::cout << std::endl;
-
-    std::cout << "size: " << v.size() << std::endl;
-    std::cout << "capacity: " << v.capacity() << std::endl;
 
     return 0;
 }
